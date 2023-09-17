d1e538a4aebb4bef061c08aa606530f1e1bece7f
epifaniofrancisco <epifaniofrancisco03@gmail.com>
2023-09-17 21:40:39 +0100

    feat: adiciona a verificação do código do país no iban

--- lib/angola_validator_extension.dart
+++ lib/angola_validator_extension.dart
@@ -7,6 +7,38 @@ extension AngolaValidatorExtension on String {
     return RegExp(regex).hasMatch(this);
   }
 
+  bool isAngolanBankCode(String bankCode) {
+    Map<String, String> angolanBanks = {
+      "BAI": "0040",
+      "BCA": "0043",
+      "BCI": "0005",
+      "BCGA": "0004",
+      "BCS": "0070",
+      "BDA": "0054",
+      "BE": "0045",
+      "BFA": "0006",
+      "BCH": "0059",
+      "BIC": "0051",
+      "BIR": "0067",
+      "BMA": "0055",
+      "BMF": "0048",
+      "BNI": "0052",
+      "BOCLB": "0071",
+      "BPC": "0010",
+      "BPG": "0064",
+      "BSOL": "0044",
+      "BVB": "0062",
+      "FNB": "0058",
+      "KEVE": "0047",
+      "SBA": "0060",
+      "SCBA": "0063",
+      "VTB": "0056",
+      "YETU": "0066"
+    };
+
+    return angolanBanks.containsValue(bankCode);
+  }
+
   String validateIBAN() {
     String iban = replaceAll(".", "").replaceAll(" ", "");
 
@@ -24,6 +56,12 @@ extension AngolaValidatorExtension on String {
 
     if (!iban.substring(4).contains(RegExp(r'^[0-9]+$'))) {
       return "IBAN inválido. Não deve conter letras após AO06";
+    }
+
+    String bankCode = iban.substring(4, 8);
+
+    if (!isAngolanBankCode(bankCode)) {
+      return "IBAN inválido. O código desse banco é inválido";
     }
 
     if (_parseIBAN(iban) != 1) {
